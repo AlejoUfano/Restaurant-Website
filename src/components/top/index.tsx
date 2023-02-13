c3ba4a2c4b1d2489cacf0c0441d0492a7b2b60dc
Alejo Ufano <ufanoalejo@gmail.com>
2023-02-13 15:08:26 -0300

    Feat(SRC) Structuring components

---
+++ src/components/top/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Top = (props: Props) => {
+  return (
+    <div>Top</div>
+  )
+}
+
+export default Top
---
+++ src/components/topProductCard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TopProductCard = (props: Props) => {
+  return (
+    <div>TopProductCard</div>
+  )
+}
+
+export default TopProductCard
