f905f7ca1f670a6c682b5d5303a03b43e0701264
Alejo Ufano <ufanoalejo@gmail.com>
2023-02-14 19:57:00 -0300

    Update(App.tsx) Components added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import Header from "./components/header";
+import Home from "./components/home";
+
 function App() {
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="app"></div>
-  )
+    <div className="app bg-[linear-gradient(90deg,var(--tw-gradient-stops))] from-[#ffedde_74%] via-[#ffedde_13%] to-[#fe043c_13%]">
+      <Header isTopOfPage={isTopOfPage} />
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
