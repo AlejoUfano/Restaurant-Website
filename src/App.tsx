cd74d33f6e804e61db8243e0d4482f938ebf6fab
Alejo Ufano <ufanoalejo@gmail.com>
2023-02-15 17:16:57 -0300

    Update(General) App.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "./components/header";
 import Home from "./components/home";
+import Top from "./components/top";
 
 function App() {
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
@@ -20,6 +21,7 @@ function App() {
     <div className="app bg-[linear-gradient(90deg,var(--tw-gradient-stops))] from-[#ffedde_74%] via-[#ffedde_13%] to-[#fe043c_13%]">
       <Header isTopOfPage={isTopOfPage} />
       <Home />
+      <Top />
     </div>
   );
 }
